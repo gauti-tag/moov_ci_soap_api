5b2b73511ed7ef40ab1ff62811ad435c2fcc5658
Gautier <gautier.tiehoule@gmail.com>
2024-03-11 15:25:29 +0000

    Feat: Setup Moov for SNEDAI

--- soap.rb
+++ soap.rb
@@ -71,6 +71,7 @@ client1 = Savon.client(
 )    
 =end 
 # xmlns:psi="http://psi.ph.floozonline.com/">
+=begin
 encode2 = "CIuvpKEsQ25skgfx0xmA6RsHxmy5i6Et+ptGlINEPq+/Or0W9bki+FzaIMeBXSa0"
   query_body2 = %Q[<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
     xmlns:psi="http://psi.ph.floozonline.com/">
@@ -107,4 +108,45 @@ client2 = Savon.client(
 #response = client.call(:send_mror , xml: query_body, soap_action: nil)
 response = client2.call(:do_cashin , xml: query_body2, soap_action: nil)
 puts response
+
+=end
+
+### SBEDAI TEST
+
+# Checkout 
+encode = "fc3X2HYaSATCNxPwG8GzriU9vqIznvmOYjFnMP5srZ1PHV9B5gfm2adgC/HTFGTX"
+query_body = %Q[<?xml version="1.0" encoding="utf-8"?>
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:onl="http://onlinepayment.tlc.com.ph/">
+    <soapenv:Header/>
+    <soapenv:Body>
+        <onl:sendMror>
+          <!--Optional:-->
+          <token>#{encode}</token>
+          <!--Optional:-->
+          <destMsisdn>2250101038387</destMsisdn>
+          <!--Optional:-->
+          <amount>200</amount>
+          <!--Optional:-->
+          <merchantRefid>QRX147TYU852Q8596123148796325874111111144477777779982369874512278</merchantRefid>
+        </onl:sendMror>
+    </soapenv:Body>
+  </soapenv:Envelope>]
+  
+client = Savon.client(
+  adapter: :net_http,
+  endpoint: 'https://merchantapi.ci-moovmoney.com/OnlinePayment/Payment?wsdl',
+  wsdl: "https://merchantapi.ci-moovmoney.com/OnlinePayment/Payment?wsdl",  #File.join("FLOOZ_PAYMENT.WSDL"),
+  ssl_verify_mode: :none,
+  ssl_cert_file: File.join("snedai/checkout/snedaiCert.pem"),
+  ssl_cert_key_file: File.join("snedai/checkout/SnedaiPrivateKey.pem"),
+  ssl_cert_key_password: "4UVm9P24HCWopcpk",
+  raise_errors: true,
+  log: true,
+  log_level: :debug,
+  pretty_print_xml: true
+)
+
+response = client.call(:send_mror , xml: query_body, soap_action: nil)
+puts response
+
 
